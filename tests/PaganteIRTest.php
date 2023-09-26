54bf1447da7915d1afb9fb1d564e2d16c34c3f49
Gustavo S. C. Joia <gustavo.joia57@gmail.com>
2023-09-25 21:59:17 -0300

    Testes de IR

--- tests/PaganteIRTest.php
+++ tests/PaganteIRTest.php
@@ -19,10 +19,34 @@ class PaganteIRTest extends TestCase{
         $this->assertEquals(48059265890, $p1->getCpf());
     }
 
-    public function testPaganteRendimento(){
-        $p1 = new PaganteIR("Gustavo","Joia",48059265890,24899);
-        
-        $this->assertEquals(24899, $p1->getRendimento());
+    public function testPaganteIsento(){
+        $p1 = new PaganteIR("Gustavo","Joia",48059265890,22800);
+        $dados = $p1->calcularDivida();
+        $this->assertEquals(0, $dados[1]);
+    }
+
+    public function testPaganteUm(){
+        $p1 = new PaganteIR("Gustavo","Joia",48059265890,22900);
+        $dados = $p1->calcularDivida();
+        $this->assertEquals(7.5, $dados[1]);
+    }
+
+    public function testPaganteDois(){
+        $p1 = new PaganteIR("Gustavo","Joia",48059265890,33950);
+        $dados = $p1->calcularDivida();
+        $this->assertEquals(15, $dados[1]);
+    }
+
+    public function testPaganteTres(){
+        $p1 = new PaganteIR("Gustavo","Joia",48059265890,46000);
+        $dados = $p1->calcularDivida();
+        $this->assertEquals(22.5, $dados[1]);
+    }
+
+    public function testPaganteQuatro(){
+        $p1 = new PaganteIR("Gustavo","Joia",48059265890,56000);
+        $dados = $p1->calcularDivida();
+        $this->assertEquals(27.5, $dados[1]);
     }
 
 }
